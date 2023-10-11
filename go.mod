b0007714587574b038c08b9f07da5dc23cebcb11
andalusia <3dsdreamcast@gmail.com>
2023-10-12 00:28:07 +0300

    fl

---
+++ go.mod
@@ -0,0 +1,3 @@
+module yha
+
+go 1.21.1
---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Creds struct {
+	Name       string
+	Surname    string
+	Patronymic string
+}
+
+func main() {
+	fmt.Println("hey")
+}
