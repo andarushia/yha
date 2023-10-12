52f2eb66880d30736d5c66158335b8b8f0029b82
andalusia <3dsdreamcast@gmail.com>
2023-10-12 18:20:23 +0300

    1984

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/lib/pq"
 )
 
 type Creds struct {
 	Name       string
 	Surname    string
 	Patronymic string
+	Age        uint64
+	Sex        string
+	Origin     string
 }
 
 func main() {
-	fmt.Println("hey")
+
 }
