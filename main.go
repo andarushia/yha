2689ab5eacb1918e7702ef93464cdc121cab95de
andalusia <3dsdreamcast@gmail.com>
2023-10-17 12:13:11 +0300

    yxa

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/jackc/pgx/v5"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -12,9 +15,17 @@ type Creds struct {
 	Name       string
 	Surname    string
 	Patronymic string
-	Age        *uint64
+	Age        *uint8
 	Sex        *string
 	Origin     *string
+}
+
+type Message struct {
+	Age    uint8  `json:"age"`
+	Sex    string `json:"gender"`
+	Origin []struct {
+		Country string `json:"country_id"`
+	} `json:"country"`
 }
 
 const (
@@ -34,15 +45,24 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	err = getName(conn)
+	err = getNameAndPopulate(conn)
 	if err != nil {
 		handleErr("couldn't get rows", err)
 	}
 }
 
-func getName(conn *pgx.Conn) error {
+func getNameAndPopulate(conn *pgx.Conn) error {
 	rows, _ := conn.Query(context.Background(), "SELECT * FROM public.profile")
-	people, err := pgx.CollectRows(rows, pgx.RowToStructByPos[Creds])
+	var person Creds
+	_, err := pgx.ForEachRow(rows, []any{&person.Id, &person.Name, &person.Surname,
+		&person.Patronymic, &person.Age, &person.Sex, &person.Origin}, func() error {
+
+		msg := Populate(person.Name)
+
+		fmt.Println(msg)
+
+		return nil
+	})
 	if err != nil {
 		return err
 	}
@@ -55,4 +75,29 @@ func handleErr(msg string, err error) {
 		fmt.Fprintf(os.Stderr, "%v: %v\n", msg, err)
 		os.Exit(1)
 	}
+}
+
+func Populate(name string) Message {
+	var msg Message
+
+	agifyUrl := fmt.Sprintf("https://api.agify.io/?name=%v", name)
+	genderizeUrl := fmt.Sprintf("https://api.genderize.io/?name=%v", name)
+	nationalizeUrl := fmt.Sprintf("https://api.nationalize.io/?name=%v", name)
+
+	getJson(agifyUrl, msg)
+	getJson(genderizeUrl, msg)
+	getJson(nationalizeUrl, msg)
+
+	return msg
+}
+
+func getJson(requestUrl string, msg Message) {
+	data, _ := http.Get(requestUrl)
+	responseBody, _ := io.ReadAll(data.Body)
+	fmt.Println(responseBody)
+	err := json.Unmarshal(responseBody, &msg)
+	if err != nil {
+		handleErr("error during requesting data", err)
+	}
+	fmt.Println(msg)
 }
